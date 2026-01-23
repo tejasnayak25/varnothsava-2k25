c50b82063873c5aadb722324b986294714266847
tejasnayak25 <arpithanayak900@gmail.com>
2026-01-23 11:20:36 +0530

    wrap ClientLayoutOverlays content in Suspense for improved loading handling

--- src/components/layout/ClientLayoutOverlays.tsx
+++ src/components/layout/ClientLayoutOverlays.tsx
@@ -2,12 +2,13 @@
 
 import { LoadingScreen } from "@/components/layout/LoadingScreen";
 import { PageTransitionBar } from "@/components/layout/PageTransitionBar";
+import { Suspense } from "react";
 
 export default function ClientLayoutOverlays() {
   return (
-    <>
+    <Suspense>
       <LoadingScreen />
       <PageTransitionBar />
-    </>
+    </Suspense>
   );
 }
